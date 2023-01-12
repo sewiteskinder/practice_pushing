2facd82f10cf352d550edcc7dd3a15929ab7b4a2
sewiteskinder <sewit.eskinder@gmail.com>
2023-01-12 09:01:59 -0600

    personal note

--- git_pushpractice.py
+++ git_pushpractice.py
@@ -1,1 +1,3 @@
 print("hi")
+print("there needs to be more time to comprehend the homework.")
+print("is it normal to not feel like i can do the work on my own? and only feel like i can do it with someone guiding me or doing the work with me?")
